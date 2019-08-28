2814f8ef34d23944cbfe54fc79b47d37acba3889
mateustasso <mateustassosiqueira@hotmail.com>
2019-08-28 20:48:30 -0300

    Deu certo

---
+++ src/main/java/drummond/com/br/DrmApiApplication.java
@@ -0,0 +1,13 @@
+package drummond.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrmApiApplication.class, args);
+	}
+
+}
